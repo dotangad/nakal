0e24c32219704e61f6dd0bdb85d27052ced1a4b4
Angad Singh <mail@angad.dev>
2020-12-28 05:16:07 +0530

    Fetch records for each table

---
+++ src/db.js
@@ -0,0 +1,34 @@
+const mysql = require("mysql");
+
+module.exports = {
+  connect: function (host, user, password, database) {
+    const conn = mysql.createConnection({
+      host,
+      user,
+      password,
+      database,
+    });
+
+    conn.connect();
+
+    return conn;
+  },
+  tables: function (conn, done) {
+    conn.query(`SHOW TABLES;`, (err, results) => {
+      if (err) return done(err);
+      done(
+        null,
+        results.map((r) => Object.values(r)).reduce((p, c) => [...p, ...c], [])
+      );
+    });
+  },
+  records: function (conn, table, done) {
+    conn.query(`SELECT * FROM ${table};`, (err, results, fields) => {
+      if (err) return done(err);
+      done(null, [results.map((r) => ({ ...r })), fields.map((f) => f.name)]);
+    });
+  },
+  end: function (conn) {
+    conn.end();
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,39 +1,61 @@
 const yargs = require("yargs");
-const { version } = require("../package.json");
+const { promisify } = require("util");
+const { connect, tables, records, end } = require("./db");
 
-module.exports = function (_args) {
-  const options = yargs
-    .usage("Usage: nakal <options> <sheet_id>")
-    .epilogue("Backup MySQL databases to Google Sheets")
-    .option("u", {
-      alias: "user",
-      describe: "MySQL Username",
-      type: "string",
-      demandOption: true,
-    })
-    .option("p", {
-      alias: "pass",
-      describe: "MySQL Password",
-      type: "string",
-    })
-    .option("db", {
-      describe: "MySQL Database",
-      type: "string",
-      demandOption: true,
-    })
-    .options("c", {
-      alias: "credentials",
-      describe: "Path to credentials.json file for access to Google's APIs.",
-      type: "string",
-      demandOption: true,
-    }).argv;
+module.exports = async function (_args) {
+  try {
+    const options = yargs
+      .usage("Usage: nakal <options> <sheet_id>")
+      .epilogue("Backup MySQL databases to Google Sheets")
+      .option("h", {
+        alias: "host",
+        describe: "MySQL host",
+        type: "string",
+        default: "127.0.0.1",
+      })
+      .option("u", {
+        alias: "user",
+        describe: "MySQL Username",
+        type: "string",
+        demandOption: true,
+      })
+      .option("p", {
+        alias: "pass",
+        describe: "MySQL Password",
+        type: "string",
+      })
+      .option("db", {
+        describe: "MySQL Database",
+        type: "string",
+        demandOption: true,
+      })
+      .options("c", {
+        alias: "credentials",
+        describe: "Path to credentials.json file for access to Google's APIs.",
+        type: "string",
+        demandOption: true,
+      }).argv;
 
-  const {
-    user,
-    pass,
-    db,
-    _: [sheetId],
-  } = options;
+    const {
+      host,
+      user,
+      pass,
+      db,
+      _: [sheetId],
+    } = options;
 
-  console.log({ user, pass, db, sheetId });
+    const conn = connect(host, user, pass, db);
+
+    const t = await promisify(tables)(conn);
+    const data = [];
+    for (let table of t) {
+      const [rows, headers] = await promisify(records)(conn, table);
+      data.push({ table, rows, headers });
+    }
+
+    end(conn);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 };
